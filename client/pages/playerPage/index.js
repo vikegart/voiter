608dee98877fa491a5c567d8bc634441fbb6bb7a
Vikentii Ekgart <Vikentii_Ekgart@epam.com>
2020-03-12 23:55:12 +0400

    верстка экрана игрока

--- client/pages/playerPage/index.js
+++ client/pages/playerPage/index.js
@@ -1,6 +1,6 @@
 const socket = io();
 
-import answerButton from './button-component';
+import { answerButton } from './button-component.mjs';
 
 const answerGroup = document.getElementsByClassName("answer-group")[0];
 const waitScreen = document.getElementsByClassName('wait-screen')[0];
@@ -24,26 +24,21 @@ const update = () => {
     }
 }
 
-const onAnswerClick = e => {
-    if (ready) {
-        window.navigator.vibrate && window.navigator.vibrate(1000);
-        ready && socket.emit("answer", name);
-    } else {
-        window.navigator.vibrate && window.navigator.vibrate([300, 100, 300]);
-    }
+const handleGroupClick = e => {
+    console.log(e);
+    // if (ready) {
+    //     window.navigator.vibrate && window.navigator.vibrate(1000);
+    //     ready && socket.emit("answer", name);
+    // } else {
+    //     window.navigator.vibrate && window.navigator.vibrate([300, 100, 300]);
+    // }
 };
 
-answerButton.addEventListener("click", onAnswerClick);
+answerGroup.addEventListener("click", handleGroupClick);
 
 socket.on('ready', state => {
     state = { ...state };
     update();
-});
-socket.on('tried', () => {
-    if (window.navigator.vibrate) {
-        window.navigator.vibrate(0);
-        window.navigator.vibrate([300, 100, 300]);
-    }
 });
 
 
